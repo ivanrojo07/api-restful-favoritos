ed6da119b4d97f05037a5d95b1deaa9901273415
Ivan Rojo <ivanrojo07@gmail.com>
2017-07-03 20:53:57 -0500

    Modificar headers content-type

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
 app.use((req, res, next )=>{
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, Control-Type, Accept, Access-Control-Request-Method');
+	res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 
